6dc16000c56a3ff7ac8d8b275f9af4ef814d6623
HsojCoding <joshhadderz@gmail.com>
2026-08-03 20:36:43 +0100

    Added & after bool to allow override

--- src/ChallengeModes.cpp
+++ src/ChallengeModes.cpp
@@ -626,7 +626,7 @@ class ChallengeMode_IronMan : public ChallengeMode
 public:
     ChallengeMode_IronMan() : ChallengeMode("ChallengeMode_IronMan", SETTING_IRON_MAN) {}
 
-    void OnPlayerResurrect(Player* player, float /*restore_percent*/, bool /*applySickness*/) override
+    void OnPlayerResurrect(Player* player, float /*restore_percent*/, bool& /*applySickness*/) override
     {
         if (!sChallengeModes->challengeEnabledForPlayer(SETTING_IRON_MAN, player))
         {
